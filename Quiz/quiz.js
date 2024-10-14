a809419a14851408ce289e9f3e1f2f4894a5e718
[Vinh Ngo] <vinhngo1996@yahoo.com>
2024-10-13 19:45:15 -0500

    Update quiz.js

--- Quiz/quiz.js
+++ Quiz/quiz.js
@@ -1,24 +1,20 @@
-//Declaring variables for points 
+var questions = ["What color is a Firetruck?", "What planet are we on?", "How many days are there in a year?"];
+var answers = ["Red", "Earth", "365"];
+var attempts = 3;
 var points = 0;
-//Setting questions and answers into one array 
-var questions = [
-    ["What color is a Firetruck?", Red],  
-    ["What planet do we live on?", Earth],
-    ["How many days are there in a year?", 365]
-];
 
-//Function that matches users answer with the correct answer and assigns points accordingly
-function askQuestion(question) {
-    var answer = prompt(question[0], "");
-    if (answer == question[1]) {
-        alert("Correct!");
-        score++;
+function quiz() {
+    for (var i = 0; i <= 3; i++) {
+        while (attempts > 0) {
+            ans = prompt("What is " + questions[0]);
+            if (ans == answers[0]) {
+                points += 1;
+                alert("Correct!");
+                attempts = 0;
+            } else {
+                alert("Incorrect");
+                attempts = attempts - 1;
+            }
+        }
     }
-    else {
-        alert("Incorrect, the answer is " + question[1]);
-    }
-}
- //For loop that cycles through the questions array 
-for (var i=0; i< questions.length; i++) {
-    askQuestion(questions[i]);
 }
